e0de868eb6f7ba2e770ec23cc0aac5da5504c4be
KolunovYuriy <y.kolunov@gmail.com>
2021-06-16 01:11:31 +0300

    gof patterns homework4 proxy some change

--- product-service/src/main/java/ru/kolumarket/product/services/ProductServiceProxy.java
+++ product-service/src/main/java/ru/kolumarket/product/services/ProductServiceProxy.java
@@ -1,15 +1,13 @@
 package ru.kolumarket.product.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import ru.kolumarket.core.dto.ProductDtoCore;
-import ru.kolumarket.product.dto.ProductDTO;
-import ru.kolumarket.product.repository.ProductRepository;
 
-import java.util.Optional;
+import java.time.Duration;
 
 @Service
 public class ProductServiceProxy {
@@ -20,13 +18,18 @@ public class ProductServiceProxy {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @Value("${kolumarket.jwt.redis_cash_expiration}")
+    private String redisCashExpiration;
+
     public Page<ProductDtoCore> getAll(Integer leftCost, Integer rightCost, Integer page, Integer size, String[] sort) {
         String key = "getPageableFromRequest_leftCost="+leftCost+ "rightCost="+rightCost+ "_page=" + page + "_size="+ size + "_sort="+sort;
         if (redisTemplate.hasKey(key)) {
             return (Page<ProductDtoCore>)redisTemplate.opsForValue().get(key);
         }
         else {
-            return productService.getAll(leftCost, rightCost, page,size,sort);
+            Page<ProductDtoCore> result = productService.getAll(leftCost, rightCost, page,size,sort);
+            redisTemplate.opsForValue().setIfAbsent(key, result, Duration.ofHours(Long.parseLong(redisCashExpiration)));
+            return result;
         }
     }
 
